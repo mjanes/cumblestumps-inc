ea3a6442275ee04f721478fcfbdcf4037bf27c1e
Matthew Janes <mjanes@gmail.com>
2014-01-25 18:47:24 -0800

    Adding readme, removing unneeded comments.

--- notd.js
+++ notd.js
@@ -1,22 +1,3 @@
-/**
- * Ok, so, I've never really written that much javascript, about time I learned it.
- *
- * So, I'm not sure if I should put all or most functionality this file, or leave it in the html page
- * that I'm writing.
- *
- * But, functions that I will need:
- *
- * // Return whatever notes have been taken for the given url
- * function String load(String url)
- *
- * // Save notes to persistent storage, with url as key
- * function void save(String url, String notes)
- *
- * Then I just need triggers to call load on the popup.html opening, and save when the save button is 
- * hit.
- */
-
-
 $(document).ready(getUrl());
 
 var url;
